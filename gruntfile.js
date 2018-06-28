e98e5d2f4cf4519fa6b565edbe0d531d51c07e3d
Jesse van Thijn <jesse@creatoins4all.nl>
2018-06-28 17:54:11 +0200

    Adding FTP push to gruntfile, install new node modules

--- gruntfile.js
+++ gruntfile.js
@@ -1,77 +1,84 @@
 // Load Grunt
 module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    // Tasks
-    sass: { // Begin Sass Plugin
-      dist: {
-        options: {
-          sourcemap: 'none'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: { // Compile everything into one task with Watch Plugin
+            css: {
+                files: ['sass/*.scss'],
+                tasks: ['sass', 'ftp_push', 'cssmin']
+            },
+            js: {
+                files: ['src/*.js'],
+                tasks: ['uglify', 'ftp_push']
+            }
         },
-        files: [{
-          expand: true,
-          cwd: 'sass',
-          src: ['**/*.scss'],
-          dest: 'css',
-          ext: '.css'
-      }]
-      }
-    },
-    postcss: { // Begin Post CSS Plugin
-      options: {
-        map: false,
-        processors: [
-      require('autoprefixer')({
-            browsers: ['last 2 versions']
-          })
-    ]
-      },
-      dist: {
-        src: 'css/style.css'
-      }
-    },
-    cssmin: { // Begin CSS Minify Plugin
-      options: {
-          keepSpecialComments: '*'
+        // Tasks
+        sass: { // Begin Sass Plugin
+            dist: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['**/*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
         },
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'css',
-          src: ['*.css', '.css'],
-          dest: 'css',
-          ext: '.css'
-    }]
-      }
-    },
-    uglify: { // Begin JS Uglify Plugin
-      build: {
-        src: ['src/*.js'],
-        dest: 'js/script.min.js'
-      }
-    },
-    watch: { // Compile everything into one task with Watch Plugin
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass', 'postcss', 'cssmin']
-      },
-      js: {
-        files: '**/*.js',
-        tasks: ['uglify']
-      }
-    }
-  });
-   
-  
-  // Load Grunt plugins
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        cssmin: { // Begin CSS Minify Plugin
+            options: {
+                keepSpecialComments: '*'
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '/'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+        uglify: { // Begin JS Uglify Plugin
+            build: {
+                src: ['src/*.js'],
+                dest: 'js/script.min.js'
+            }
+        },
+        ftp_push: {
+            your_target: {
+                options: {
+                    username: "user",
+                    password: "password",
+                    host: "server.com",
+                    dest: "/root/wp-content/themes/theme/",
+                    port: 21
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.',
+                        src: [
+                            "css/style.css",
+                            "js/script.min.js"
+                        ]
+                    }
+                ]
+            }
+        }
+
+    });
+
+    // Load Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ftp-push');
 
 
-  // Register Grunt tasks
-  grunt.registerTask('default', ['watch']);
+    // Register Grunt tasks
+    grunt.registerTask('default', ['watch']);
 };
